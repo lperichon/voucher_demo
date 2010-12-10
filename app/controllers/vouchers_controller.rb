f078ccd199b92026862c2138bd0a6abd3f4537af
Luis Perichon <luigi@luigi-laptop.(none)>
2010-12-10 15:23:24 -0300

    first commit

---
+++ app/controllers/vouchers_controller.rb
@@ -0,0 +1,7 @@
+class VouchersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoucherDemo::Application.config.secret_token = '496a9d7dc65ac73c099fea59c3f70133a57356848470f0a36c2957d14748810f7fec82a02aac5347756b9071b65099020c55c0dcd8ee94bdf239ea7d9e5b66c0'
